f87198e2e68047a13ea677d0444fbfd18ed13846
bjdumm <bjdumm45@gmail.com>
2021-12-13 19:18:32 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Test from './Test';
+import { Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div class="content">
+      <Test title="Stuff in the body of app." />
+      <Test title="second component" />
+      <Test title="Third Component" />
+      <Test title="fourth component" />
+      <Test title="Fifth component" />
+      <Test title="Sixth component" />
+      <Test title="Seventh Component" />
+      <Test title="Eigth component" />
+      <Test title="Ninth component" />
+      <Test title="Tenth component" />
+      </div>
+      <footer className="App-footer">
+        <p>Put some footer links here</p>
+      </footer>
     </div>
   );
 }
