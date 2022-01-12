3e45c086eacc6f94b06cafb0d1e65d9ea962c30c
bjdumm <bjdumm45@gmail.com>
2022-01-12 00:03:58 -0500

    "Reformated Math and Computer programming pages"

--- src/Pages/compprog.js
+++ src/Pages/compprog.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../CSS/relativity.css';
+import '../CSS/compprog.css';
 import TubeLink from '../Components/TubeLink';
 import cp1 from '../Assets/Videos/cp1.png';
 import cp2 from '../Assets/Videos/cp2.png';
@@ -13,13 +13,9 @@ class CompProg extends React.Component {
                 <hr></hr>
                 <div>
                     <h2>Tutorials</h2>
-                    <div style={{display:"flex", justifyContent: "space-around"}}>
+                    <div class="section">
                         
-                        <div>
-                        <h2>Language Tutorials</h2>
-                        <TubeLink name="C Made Simple" url="https://www.youtube.com/playlist?list=PLfqABt5AS4FmErobw8YyTwXDUE5nPH5lH" img={cp1}/>
-                        <TubeLink name="C Made Simple" url="https://www.youtube.com/playlist?list=PLfqABt5AS4FmErobw8YyTwXDUE5nPH5lH" img={cp1}/>
-                        </div>
+                
 
                         <div>
                         <h2>Data Structures and Algorithms</h2>
@@ -46,7 +42,7 @@ class CompProg extends React.Component {
 
                 <div>
                 <h2>Language Tutorials</h2>
-                    <div style={{display:"flex",justifyContent:"space-around"}}>
+                    <div class="section">
                         <TubeLink name="C Made Simple" url="https://www.youtube.com/playlist?list=PLfqABt5AS4FmErobw8YyTwXDUE5nPH5lH" img={cp1}/>
                         <TubeLink name="C Made Simple" url="https://www.youtube.com/playlist?list=PLfqABt5AS4FmErobw8YyTwXDUE5nPH5lH" img={cp1}/>
                     </div>
@@ -55,7 +51,7 @@ class CompProg extends React.Component {
 
                 <div>
                     <h2>Documentation</h2>
-                    <div style={{display:"flex",justifyContent:"space-around"}}>
+                    <div class="section">
                     <TubeLink name="MDN Docs" url="https://developer.mozilla.org/en-US/docs/Web" img={cp2}/>    
                     </div>
                 </div>
--- src/Pages/math.js
+++ src/Pages/math.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../CSS/relativity.css';
+import '../CSS/math.css';
 import TubeLink from '../Components/TubeLink';
 import math1 from '../Assets/Videos/math1.png';
 
@@ -12,17 +12,26 @@ class Math extends React.Component {
                 <h1 style={{color:"green"}}>General Relativity</h1>
                 <hr></hr>
                 <div>
-                    <h2>YouTube</h2>
-                    <div style={{display:"flex", justifyContent: "space-around"}}>
+                    <h2>General Math</h2>
+                    <div class="section">
                         <TubeLink name="Math Exposition" url="https://www.youtube.com/playlist?list=PLnQX-jgAF5pTkwtUuVpqS5tuWmJ-6ZM-Z" img={math1}/>
-                        <TubeLink name="DSP" url="https://www.youtube.com/playlist?list=PLuh62Q4Sv7BUSzx5Jr8Wrxxn-U10qG1et" img={math1}/>
+                        <TubeLink name="3B1B" url="https://www.youtube.com/playlist?list=PLuh62Q4Sv7BUSzx5Jr8Wrxxn-U10qG1et" img={math1}/>
                         <TubeLink name="DSP" url="https://www.youtube.com/playlist?list=PLuh62Q4Sv7BUSzx5Jr8Wrxxn-U10qG1et" img={math1}/>
                         <TubeLink name="DSP" url="https://www.youtube.com/playlist?list=PLuh62Q4Sv7BUSzx5Jr8Wrxxn-U10qG1et" img={math1}/>
                         
-                        
-                        
+                         
                     </div>
                     <hr></hr>
+                    <h2>Calculus/Differential Equations</h2>
+                    <div class="section">
+                        <TubeLink name="Math Exposition" url="https://www.youtube.com/playlist?list=PLnQX-jgAF5pTkwtUuVpqS5tuWmJ-6ZM-Z" img={math1}/>
+                        <TubeLink name="3B1B" url="https://www.youtube.com/playlist?list=PLuh62Q4Sv7BUSzx5Jr8Wrxxn-U10qG1et" img={math1}/>
+                        <TubeLink name="DSP" url="https://www.youtube.com/playlist?list=PLuh62Q4Sv7BUSzx5Jr8Wrxxn-U10qG1et" img={math1}/>
+                        <TubeLink name="DSP" url="https://www.youtube.com/playlist?list=PLuh62Q4Sv7BUSzx5Jr8Wrxxn-U10qG1et" img={math1}/>
+                        
+                         
+                    </div>
+
                 </div>
                 <div>
                     <h2>Site Links</h2>
